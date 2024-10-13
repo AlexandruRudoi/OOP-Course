8b547d7e47b6ecae931a9a24417505689cc35d00
Alexandru Rudoi <rudoi.alexandru25@gmail.com>
2024-10-13 23:16:18 +0300

    add OutputWriter

---
+++ Lab_0/Lab_0/OutputWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using Lab_0.Domain;
+
+namespace Lab_0;
+
+public class OutputWriter
+{
+    public void WriteToFile(string fileName, string universeName, List<AlienSpecies> individuals)
+    {
+        var outputDirectory = "../../../output";
+        if (!Directory.Exists(outputDirectory))
+        {
+            Directory.CreateDirectory(outputDirectory);
+        }
+
+        var universeOutput = new
+        {
+            name = universeName,
+            individuals = individuals
+        };
+
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string jsonString = JsonSerializer.Serialize(universeOutput, options);
+
+        // Write the output to a JSON file
+        File.WriteAllText(Path.Combine(outputDirectory, fileName), jsonString);
+
+        Console.WriteLine($"{fileName} has been written with {individuals.Count} individuals.");
+    }
+}
--- Lab_0/Lab_0/Program.cs
+++ Lab_0/Lab_0/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Lab_0.Domain;
+﻿using Lab_0.Domain;
 
 namespace Lab_0;
 
@@ -8,17 +7,44 @@ internal class Program
     static void Main(string[] args)
     {
         JsonReader jsonReader = new JsonReader();
+        OutputWriter outputWriter = new OutputWriter(); 
+        
         string jsonFilePath = "../../../input/input.json";
         List<AlienSpecies> alienSpeciesList = jsonReader.ReadJson(jsonFilePath);
 
         if (alienSpeciesList != null)
         {
+            List<AlienSpecies> hitchhikerList = new List<AlienSpecies>();
+            List<AlienSpecies> marvelList = new List<AlienSpecies>();
+            List<AlienSpecies> ringsList = new List<AlienSpecies>();
+            List<AlienSpecies> starwarsList = new List<AlienSpecies>();
+
             foreach (var species in alienSpeciesList)
             {
-                species.PrintSpecies();
-                Console.WriteLine($"Classified as: {species.Classify()}");
-                Console.WriteLine("-------------------------------");
+                string classification = species.Classify();
+
+                if (classification.Contains("Hitchhiker"))
+                {
+                    hitchhikerList.Add(species);
+                }
+                else if (classification.Contains("Marvel"))
+                {
+                    marvelList.Add(species);
+                }
+                else if (classification.Contains("Rings") || classification.Contains("Lord of the Rings"))
+                {
+                    ringsList.Add(species);
+                }
+                else if (classification.Contains("Star Wars"))
+                {
+                    starwarsList.Add(species);
+                }
             }
+
+            outputWriter.WriteToFile("hitchhiker.json", "hitchHiker", hitchhikerList);
+            outputWriter.WriteToFile("marvel.json", "marvel", marvelList);
+            outputWriter.WriteToFile("rings.json", "rings", ringsList);
+            outputWriter.WriteToFile("starwars.json", "starwars", starwarsList);
         }
     }
 }
