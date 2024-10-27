405199bae9637c8c7d9818e57ecef742f0ebb0d8
Alexandru Rudoi <rudoi.alexandru25@gmail.com>
2024-10-27 20:49:00 +0200

    add Display class

---
+++ Lab_1/Lab_1/Display.cs
@@ -0,0 +1,61 @@
+﻿namespace Lab_1;
+
+public class Display
+{
+    private int Width { get; set; }
+    private int Height { get; set; }
+    private float Ppi { get; set; }
+    private string Model { get; set; }
+
+    public Display(int width, int height, float ppi, string model)
+    {
+        this.Width = width;
+        this.Height = height;
+        this.Ppi = ppi;
+        this.Model = model;
+    }
+
+    private int Area()
+    {
+        return Width * Height;
+    }
+
+    public void CompareSize(Display other)
+    {
+        if (this.Area() > other.Area())
+        {
+            Console.WriteLine($"{this.Model} is bigger in size than {other.Model}.");
+        }
+        else if (this.Area() < other.Area())
+        {
+            Console.WriteLine($"{this.Model} is smaller in size than {other.Model}.");
+        }
+        else
+        {
+            Console.WriteLine($"{this.Model} and {other.Model} are equal in size.");
+        }
+    }
+
+    public void CompareSharpness(Display other)
+    {
+        if (this.Ppi > other.Ppi)
+        {
+            Console.WriteLine($"{this.Model} is sharper than {other.Model}.");
+        }
+        else if (this.Ppi < other.Ppi)
+        {
+            Console.WriteLine($"{this.Model} is less sharp than {other.Model}.");
+        }
+        else
+        {
+            Console.WriteLine($"{this.Model} and {other.Model} have the same sharpness.");
+        }
+    }
+
+    public void CompareWithMonitor(Display other)
+    {
+        Console.WriteLine($"Comparing {this.Model} with {other.Model}:");
+        CompareSize(other);
+        CompareSharpness(other);
+    }
+}
--- Lab_1/Lab_1/Program.cs
+++ Lab_1/Lab_1/Program.cs
@@ -4,6 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        // Create three Display objects
+        Display display1 = new Display(1920, 1080, 401.5f, "Display One");
+        Display display2 = new Display(2560, 1440, 350.0f, "Display Two");
+        Display display3 = new Display(3840, 2160, 500.0f, "Display Three");
+
+        // Compare displays
+        display1.CompareWithMonitor(display2);
+        display2.CompareWithMonitor(display3);
+        display1.CompareWithMonitor(display3);
     }
 }
