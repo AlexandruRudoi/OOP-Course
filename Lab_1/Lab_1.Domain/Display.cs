00f7ad4fb95ac668b6cf9f6ce78f5a213dcc14ba
Alexandru Rudoi <rudoi.alexandru25@gmail.com>
2024-10-27 22:15:57 +0200

    add documentation

--- Lab_1/Lab_1.Domain/Display.cs
+++ Lab_1/Lab_1.Domain/Display.cs
@@ -2,11 +2,33 @@
 
 public class Display
 {
+    /// <summary>
+    /// Gets or sets the width of the display in pixels.
+    /// </summary>
     private int Width { get; set; }
+
+    /// <summary>
+    /// Gets or sets the height of the display in pixels.
+    /// </summary>
     private int Height { get; set; }
+
+    /// <summary>
+    /// Gets or sets the pixel density (pixels per inch) of the display.
+    /// </summary>
     private float Ppi { get; set; }
+
+    /// <summary>
+    /// Gets or sets the model name of the display.
+    /// </summary>
     private string Model { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Display"/> class with specified dimensions, pixel density, and model.
+    /// </summary>
+    /// <param name="width">The width of the display in pixels.</param>
+    /// <param name="height">The height of the display in pixels.</param>
+    /// <param name="ppi">The pixel density (ppi) of the display.</param>
+    /// <param name="model">The model name of the display.</param>
     public Display(int width, int height, float ppi, string model)
     {
         this.Width = width;
@@ -15,11 +37,19 @@ public class Display
         this.Model = model;
     }
 
+    /// <summary>
+    /// Calculates the area of the display in pixels.
+    /// </summary>
+    /// <returns>The area of the display (width * height).</returns>
     private int Area()
     {
         return Width * Height;
     }
 
+    /// <summary>
+    /// Compares the size of this display with another display based on area.
+    /// </summary>
+    /// <param name="other">The other <see cref="Display"/> object to compare with.</param>
     public void CompareSize(Display other)
     {
         if (this.Area() > other.Area())
@@ -36,6 +66,10 @@ public class Display
         }
     }
 
+    /// <summary>
+    /// Compares the sharpness of this display with another display based on pixel density (ppi).
+    /// </summary>
+    /// <param name="other">The other <see cref="Display"/> object to compare with.</param>
     public void CompareSharpness(Display other)
     {
         if (this.Ppi > other.Ppi)
@@ -52,6 +86,10 @@ public class Display
         }
     }
 
+    /// <summary>
+    /// Compares both the size and sharpness of this display with another display, providing a detailed output.
+    /// </summary>
+    /// <param name="other">The other <see cref="Display"/> object to compare with.</param>
     public void CompareWithMonitor(Display other)
     {
         Console.WriteLine($"Comparing {this.Model} with {other.Model}:");
